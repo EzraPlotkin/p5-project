9e70d476565988f361bfcf03d9009f8a464f71e3
Ezra Plotkin <ezrajplotkin@gmail.com>
2021-11-23 17:35:22 -0500

    added cycle function to main.js and added .ds_store to git ignore

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const setup = (s) => () => {
     s.background(0)
 }
 
+function cycle(x, within) {
+    return x%within
+}
+
 let i = 0;
 const draw = (s) => () => {
     s.fill(100);
-    s.rect(i%400, i%400, 20, 20);
+    s.rect(cycle(i,400), cycle( i,400), 20, 20);
     i += 1;
 }
 
